cb41565c349b3470f2d80a4507d30c78a1640da8
codegeeks98 <codegeeks98@gmail.com>
2025-06-11 23:14:20 +0530

    second commit

--- stocks/urls.py
+++ stocks/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include('quotes.urls')),   #To use the "urls.py" file of the "qoute" app
 ]
